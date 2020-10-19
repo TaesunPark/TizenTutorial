ae7f55a5ee26d22b4ee097a224a0612e29ad9055
taesunpark <tovbskvb@daum.net>
2020-10-20 01:51:37 +0900

    [add] accelator cencor add

---
+++ sensoraccelerator/inc/sensoraccelerator.h
@@ -0,0 +1,19 @@
+#ifndef __sensoraccelerator_H__
+#define __sensoraccelerator_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "sensoraccelerator"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.sensoraccelerator"
+#endif
+
+#endif /* __sensoraccelerator_H__ */
