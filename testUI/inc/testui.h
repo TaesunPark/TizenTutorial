74f8716e2afa119f3d230f8c603266e0ad6f9375
taesunpark <tovbskvb@daum.net>
2020-10-20 00:32:14 +0900

    [add] add tutorial

---
+++ testUI/inc/testui.h
@@ -0,0 +1,19 @@
+#ifndef __testui_H__
+#define __testui_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "testui"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.testui"
+#endif
+
+#endif /* __testui_H__ */
